411d5513fadf4faa2ae9dedcfc11d24f1d68b6cb
Tumelo Segale <tumelosegale56@gmail.com>
2025-08-12 11:10:58 +0200

    functions

--- functions.py
+++ functions.py
@@ -1,1 +1,21 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
+def add():
+    sum_result = num1 + num2
+    return sum_result
+
+def multiply():
+    product_result = num1 * num2
+    return product_result
+
+def divide():
+    if num2 == 0 or num1 == 0:
+        return "Error: Division by zero is not allowed."
+    quotient_result = num1 / num2
+    return quotient_result
+
+print("Addition:", add())
+print("Multiplication:", multiply())
+print("Division:", divide())
+print("-----------------")
